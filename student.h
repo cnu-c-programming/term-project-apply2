f524c21d6d2d75f69ff9fb1b1fd53e6cf9c0f09f
apply2 <67138665+apply2@users.noreply.github.com>
2026-06-01 14:11:37 +0900

    2026/06/01/14:11

--- student.h
+++ student.h
@@ -1,25 +1,15 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <stdio.h>
 typedef struct Student {
     int id;
-    char* name;
+    char name[32];
     int score;
     struct Student* next;
 } Student;
-void addStudent(Student** head, int id, char* name, int score){
-    Student* newStudent = (Student*)malloc(sizeof(Student));
-    newStudent->id = id;
-    strcpy(newStudent->name, name);
-    newStudent->score = score;
-    newStudent->next = NULL;
-    *head = newStudent;
-    if(*head == NULL) {
-        *head = newStudent;
-        return;
-    }
-    
-};
-void printStudents(Student* head) {
-    
-};
-void freeStudents(Student* head) {
-
-};
+void addStudent(Student** head, int id, char* name, int score);
+void listStudents(Student* head);
+void printStudents(Student* head);
+void deleteStudent(Student** head, int id);
+void findStudent(Student** head, int id);
+#endif
