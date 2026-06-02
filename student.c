5ef68b0ae12877517c733a8c7b6d7733d7394fef
apply2 <67138665+apply2@users.noreply.github.com>
2026-06-02 16:26:00 +0900

    2026/06/02/16:25

--- student.c
+++ student.c
@@ -8,25 +8,99 @@ void addStudent(Student** head, int id, char* name, int score) {
     if (newStudent == NULL) {
         fprintf(stderr, "Error: Memory allocation failed\n");
         return;
-    };
+    }
+
     newStudent->id = id;
-    strcpy(newStudent->name, name, sizeof(newStudent->name) - 1);
+    strncpy(newStudent->name, name, sizeof(newStudent->name) - 1);
     newStudent->name[sizeof(newStudent->name) - 1] = '\0';
     newStudent->score = score;
     newStudent->next = NULL;
+
+    Student* temp = *head;
+    while (temp != NULL) {
+        if (temp->id == id) {
+            printf("Error: duplicate ID\n");
+            free(newStudent);
+            return;
+        }
+        temp = temp->next;
+    }
+
+    if(*head == NULL) {
+        *head = newStudent;
+    }
+    else {
+        temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newStudent;
+    }
+    printf("Student added\n");
 }
 void listStudents(Student* head) {
+    Student* temp = head;
 
-};
+    if (head == NULL) {
+        printf("No students found\n");
+        return;
+    }
+
+    printf("% 5s % 15s % 5s\n", "ID", "Name", "Score");
+    while (temp != NULL) {
+        printf("% 5d % 15s % 5d\n", temp->id, temp->name, temp->score);
+        temp = temp->next;
+    }
+}
 void deleteStudent(Student** head, int id) {
+    Student *temp = *head, *prev = NULL;
+    if (*head == NULL) {
+        printf("ERROR: student not found\n");
+        return;
+    }
+    while (temp != NULL) {
+        if (temp -> id == id) {
+            if (prev == NULL) *head = temp -> next;
+            else prev->next = temp->next;
 
-};
-void updateStudent(Student** head, int id, int score) {
-    
-};
-void findStudent(Student** head, int id) {
-    
-};
+            free(temp);
+            printf("Student deleted\n");
+            return;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    printf("ERROR: student not found\n");
+}
+void updateStudent(Student* head, int id, int newScore) {
+    Student* temp = head;
+    while (temp != NULL) {
+        if (temp->id == id) {
+            temp->score = newScore;
+            printf("Student updated\n");
+            return;
+        }
+        temp = temp->next;
+    }
+    printf("ERROR: student not found\n");
+}
+void findStudent(Student* head, int id) {
+    Student* temp = head;
+    while (temp != NULL) {
+        if (temp->id == id) {
+            printf("ID: %d\n", temp->id);
+            printf("Name: %s\n", temp->name);
+            printf("Score: %d\n", temp->score);
+            return;
+        }
+        temp = temp->next;
+    }
+    printf("ERROR: student not found\n");
+}
 void freeStudents(Student* head) {
-    
-};
+    while (head) {
+        Student* tmp = head;
+        head = head->next;
+        free(tmp);
+    }
+}
--- student.h
+++ student.h
@@ -12,8 +12,8 @@ typedef struct Student {
 void addStudent(Student** head, int id, char* name, int score);
 void listStudents(Student* head);
 void deleteStudent(Student** head, int id);
-void updateStudent(Student** head, int id, int score);
-void findStudent(Student** head, int id);
+void updateStudent(Student* head, int id, int score);
+void findStudent(Student* head, int id);
 void freeStudents(Student* head);
 
 #endif
