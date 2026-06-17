1e046ef6d48bcd196a0b022daf4e7652ebfd127d
apply2 <67138665+apply2@users.noreply.github.com>
2026-06-17 10:41:05 +0900

    2026/06/17/10:40

--- command.c
+++ command.c
@@ -7,104 +7,147 @@
 
 extern char* globalfilename;
 
-shellResult handlerList(char* args, Student** head) {
+ShellResult handle_list(char* args, Student** head) {
     if (*head == NULL) {
         printf("No students found.\n");
     } else {
         listStudents(*head);
     }
-    return CMD_SUCCESS;
+    return SHELL_OK;
 }
 
-shellResult handlerFind(char* args, Student** head) {
+ShellResult handle_find(char* args, Student** head) {
     char* idStr = strtok(NULL, " \t\n");
     if (!idStr) {
         printf("Error: missing ID.\n");
-        return CMD_ERROR;
+        return SHELL_ERR_MISSING_ARGUMENT;
     }
     findStudent(*head, atoi(idStr));
-    return CMD_SUCCESS;
+    return SHELL_OK;
 }
 
-shellResult handlerReload(char* args, Student** head) {
+ShellResult handle_reload(char* args, Student** head) {
     freeStudents(*head);
     *head = NULL;
-    int count = loadStudentsFromCSV(globalfilename, head);
+    int count = loadStudent(globalfilename, head);
     if (count >= 0) printf("Reloaded %d students from %s.\n", count, globalfilename);
-    return CMD_SUCCESS;
+    return SHELL_OK;
 }
 
-shellResult handlerExit(char* args, Student** head) {
+ShellResult handle_stats(char* args, Student** head) {
+    printf("Statistics displayed.\n");
+    return SHELL_OK;
+}
+
+ShellResult handle_clear(char* args, Student** head) {
+    system("clear || cls");
+    return SHELL_OK;
+}
+
+ShellResult handle_exit(char* args, Student** head) {
     printf("Goodbye.\n");
     freeStudents(*head);
-    return CMD_EXIT;
+    return SHELL_EXIT;
 }
-//어드민 모드
+
+ShellResult handle_help(char* args, Student** head); 
+
+
 #ifdef ADMIN_MODE
-shellResult handlerAdd(char* args, Student** head) {
+
+ShellResult handle_add(char* args, Student** head) {
     char* idStr = strtok(NULL, " \t\n");
     char* nameStr = strtok(NULL, " \t\n");
     char* scoreStr = strtok(NULL, " \t\n");
-
+    
     if (!idStr || !nameStr || !scoreStr) {
         printf("Error: missing arguments.\n");
-        return CMD_ERROR;
+        return SHELL_ERR_MISSING_ARGUMENT;
     }
     addStudent(head, atoi(idStr), nameStr, atoi(scoreStr));
-    return CMD_SUCCESS;
+    
+    return SHELL_OK;
 }
 
-shellResult handlerDelete(char* args, Student** head) {
+ShellResult handle_delete(char* args, Student** head) {
     char* idStr = strtok(NULL, " \t\n");
     if (!idStr) {
         printf("Error: missing ID.\n");
-        return CMD_ERROR;
+        return SHELL_ERR_MISSING_ARGUMENT;
     }
     deleteStudent(head, atoi(idStr));
-    return CMD_SUCCESS;
+    return SHELL_OK;
 }
 
-shellResult handlerUpdate(char* args, Student** head) {
+ShellResult handle_update(char* args, Student** head) {
     char* idStr = strtok(NULL, " \t\n");
     char* scoreStr = strtok(NULL, " \t\n");
+    
     if (!idStr || !scoreStr) {
         printf("Error: missing arguments.\n");
-        return CMD_ERROR;
+        return SHELL_ERR_MISSING_ARGUMENT;
     }
     updateStudent(*head, atoi(idStr), atoi(scoreStr));
-    return CMD_SUCCESS;
+    return SHELL_OK;
 }
 
-shellResult handlerSave(char* args, Student** head) {
-    int count = saveStudentsToCSV(globalfilename, *head);
+ShellResult handle_save(char* args, Student** head) {
+    int count = saveStudent(globalfilename, *head);
     if (count >= 0) printf("Saved %d students to %s.\n", count, globalfilename);
-    return CMD_SUCCESS;
+    return SHELL_OK;
 }
+
 #endif
 
-static Command carr[] = {
-    {"list",   handlerList,   "list", "Show all students"},
-    {"find",   handlerFind,   "find <id>", "Find student by ID"},
-    {"reload", handlerReload, "reload", "Reload data from CSV"},
 #ifdef ADMIN_MODE
-    {"add",    handlerAdd,    "add <id> <name> <score>", "Add a student"},
-    {"delete", handlerDelete, "delete <id>", "Delete a student"},
-    {"update", handlerUpdate, "update <id> <score>", "Update student score"},
-    {"save",   handlerSave,   "save", "Save data to CSV"},
-#endif
-    {"exit",   handlerExit,   "exit", "Exit program"}
+Command commands[] = {
+    {"save",   handle_save,   "save",                    "Save students to CSV"},
+    {"reload", handle_reload, "reload",                  "Reload students from CSV"},
+    {"add",    handle_add,    "add <id> <name> <score>", "Add a student"},
+    {"delete", handle_delete, "delete <id>",             "Delete a student"},
+    {"update", handle_update, "update <id> <score>",     "Update student score"},
+    {"find",   handle_find,   "find <id>",               "Find student"},
+    {"list",   handle_list,   "list",                    "List students"},
+    {"stats",  handle_stats,  "stats",                   "Show statistics"},
+    {"help",   handle_help,   "help",                    "Show help"},
+    {"clear",  handle_clear,  "clear",                   "Clear screen"},
+    {"exit",   handle_exit,   "exit",                    "Exit shell"}
 };
+#endif
 
-shellResult processingcommand(char* input, Student** head) {
-    char* cmdName = strtok(input, " \t\n");
-    if (cmdName == NULL) return CMD_SUCCESS;
+#ifdef CLIENT_MODE
+Command commands[] = {
+    {"reload", handle_reload, "reload",      "Reload students from CSV"},
+    {"find",   handle_find,   "find <id>",   "Find student"},
+    {"list",   handle_list,   "list",        "List students"},
+    {"stats",  handle_stats,  "stats",       "Show statistics"},
+    {"help",   handle_help,   "help",        "Show help"},
+    {"clear",  handle_clear,  "clear",       "Clear screen"},
+    {"exit",   handle_exit,   "exit",        "Exit shell"}
+};
+#endif
 
-    int nums = sizeof(carr) / sizeof(Command);
-    for (int i = 0; i < nums; i++) {
-        if (strcmp(cmdName, carr[i].name) == 0) {
-            return carr[i].handler(NULL, head);
+const int num_commands = sizeof(commands) / sizeof(Command);
+
+ShellResult handle_help(char* args, Student** head) {
+    printf("\nAvailable Commands:\n");
+    for (int i = 0; i < num_commands; i++) {
+        printf("  %-25s : %s\n", commands[i].usage, commands[i].description);
+    }
+    printf("\n");
+    return SHELL_OK;
+}
+
+ShellResult process_command(char* input, Student** head) {
+    char* cmd = strtok(input, " \t\n");
+    if (cmd == NULL) {
+        return SHELL_OK; 
+    }
+    for (int i = 0; i < num_commands; i++) {
+        if (strcmp(cmd, commands[i].name) == 0) {
+            return commands[i].handler(NULL, head);
         }
     }
     printf("Unknown command or permission denied.\n");
-    return CMD_ERROR;
+    return SHELL_ERR_UNKNOWN_COMMAND;
 }
